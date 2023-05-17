deea315614cbdcc02c14bcf1a7d1391f8ec6c8a7
BrunoFerroTs <87425146+brunoferrots@users.noreply.github.com>
2023-05-16 21:23:23 -0300

    For Basic

---
+++ src/basics/For1.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class For1 {
+    public static void main(String[] args) {
+        int[] numberList = new int[] {3, 5, 7, 9, 11};
+
+        for (int number : numberList) {
+            System.out.println(number);
+        }
+    }
+}
